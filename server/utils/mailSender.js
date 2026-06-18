e9895ed8b3ce3f3c12c2be8548aeec6eacc396a0
Somyabhawsar45 <Somyabhawsar194@gmail.com>
2026-06-18 13:03:31 +0530

    final working on local host

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -1,36 +1,27 @@
-const dns = require("dns")
-dns.setDefaultResultOrder("ipv4first")
-
 const nodemailer = require("nodemailer")
 
 const mailSender = async (email, title, body) => {
   try {
     let transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: process.env.MAIL_HOST,
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
       },
-      connectionTimeout: 30000,
-      greetingTimeout: 30000,
-      socketTimeout: 30000,
+      secure: false,
     })
-
-    await transporter.verify()
-    console.log("SMTP Connected")
 
     let info = await transporter.sendMail({
-      from: `"Schlor | Somya" <${process.env.MAIL_USER}>`,
-      to: email,
-      subject: title,
-      html: body,
+      from: `"Schlor | Somya" <${process.env.MAIL_USER}>`, // sender address
+      to: `${email}`, // list of receivers
+      subject: `${title}`, // Subject line
+      html: `${body}`, // html body
     })
-
-    console.log("Email sent successfully:", info.response)
+    console.log(info.response)
     return info
   } catch (error) {
-    console.error("Email error:", error)
-    throw error
+    console.log(error.message)
+    return error.message
   }
 }
 
